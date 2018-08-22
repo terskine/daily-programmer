17a80c13c54887d1099b15b29ccad7ef9d1a8fa1
terskine <tom.erskine91@gmail.com>
2018-08-22 15:53:12 +0000

    inital try at hyphenation

---
+++ hyphenation/hyphenation.py
@@ -0,0 +1,40 @@
+import sys
+import re
+
+def hyphenate(p, word):
+    
+    places = [0] * (len(word) + 1)
+    
+    for x in p:
+        for y in re.finditer(x[0],word):
+            for z in x[1]:
+                if x[1][z] > places[y.start()+z]:
+                    places[y.start()+z] = x[1][z]
+    hyph = ''
+    for idex,x in enumerate(word):
+        if places[idex] % 2 == 1:
+            hyph += '-'
+        hyph += x
+    return hyph
+    
+def createDict():
+    patterns = [re.sub('\\.$','$', y) for y in [re.sub('^\\.','^', x) for x in [line.strip('\n').replace('^.','^').replace('.$','$') for line in open('dict.txt')]]]
+    numRegex = re.compile('[0-9]+')
+    p=[]
+    for x in patterns:
+        a={}
+        c=0
+        for y in re.finditer(numRegex,x):
+            a[y.start()-c] = int(y.group(0))
+            c += 1
+        p.append((re.sub(numRegex,'',x), a))
+    return p
+
+if __name__== "__main__":
+        
+    p = createDict()        
+    word = sys.argv[1]
+    print hyphenate(p,word)
+    
+
+        
